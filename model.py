66b553911d7167e9054779a8f73590b122783224
SirHacksalott <43313663+SirHacksalott@users.noreply.github.com>
2021-06-28 01:12:46 +0530

    Model Buiding done

--- model.py
+++ model.py
@@ -13,8 +13,8 @@ df = pd.read_csv('edu_data.csv')
 
 # choose relevant columns
 
-df_model = df[['Rating','Size','Type of ownership', 'Industry', 'Sector', 'Revenue',
-        'avg_salary','hourly','employer_provided','num_comp','job_state', 'same_state', 'age', 'python_yn',
+df_model = df[['avg_salary','Rating','Size','Type of ownership', 'Industry', 'Sector', 'Revenue','num_comp',
+        'hourly','employer_provided','job_state', 'same_state', 'age', 'python_yn',
        'SparkR_yn', 'aws_yn', 'Excel_yn','seniority','desc_len','job_simp']]
 
 # get dummy variables
@@ -30,22 +30,57 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Multiple Linear Regression
 import statsmodels.api as sm
 
-X_sm = sm.add_constant(X)
-model = sm.OLS(y,X_sm).fit()
-print(model.summary())
+X_sm =X= sm.add_constant(X)
+model = sm.OLS(y,X_sm)
+model.fit().summary()
 
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression,Lasso
 from sklearn.model_selection import cross_val_score
 
 lm = LinearRegression()
 lm.fit(X_train,y_train)
 
-cross_val_score(lm,X_train,y_train,scoring='neg_mean_absolute_error',cv=3)
+np.mean(cross_val_score(lm,X_train,y_train,scoring='neg_mean_absolute_error',cv=3))
 
 # Lasso Regression
+lm_l = Lasso(alpha=0.14)
+lm_l.fit(X_train,y_train)
+np.mean(cross_val_score(lm_l,X_train,y_train,scoring='neg_mean_absolute_error',cv=3))
+
+alpha = []
+error = []
+for i in range(1,100):
+    alpha.append(i/100)
+    lml = Lasso(alpha=i/100)
+    error.append(np.mean(cross_val_score(lml,X_train,y_train,scoring='neg_mean_absolute_error',cv=3)))
+
+plt.plot(alpha,error)
+
+err = tuple(zip(alpha,error))
+df_err = pd.DataFrame(err,columns=['alpha','error'])
+df_err[df_err.error==max(df_err.error)]
+
 # Random Forrest
+from sklearn.ensemble import RandomForestRegressor
+rf = RandomForestRegressor()
+np.mean(cross_val_score(rf,X_train,y_train,scoring='neg_mean_absolute_error',cv=3))
+
 # tune models GridSearchCV
+from sklearn.model_selection import GridSearchCV
+parameters = {'n_estimators':range(10,300,10),'criterion':('mse','mae'),'max_features':('auto','sqrt','log2')}
+
+gs = GridSearchCV(rf,parameters,scoring='neg_mean_absolute_error',cv = 3)
+gs.fit(X_train,y_train)
+
+gs.best_score_
+gs.best_params_
+
 # test ensembles
+tpred_lm = lm.predict(X_test)
+tpred_lml = lm_l.predict(X_test)
+tpred_rf = gs.best_estimator_.predict(X_test)
 
-
-
+from sklearn.metrics import mean_absolute_error
+mean_absolute_error(y_test,tpred_lm)
+mean_absolute_error(y_test,tpred_lml)
+mean_absolute_error(y_test,tpred_rf)
